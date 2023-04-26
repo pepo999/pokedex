382b729c3bcb3a8db0ad2e0ff19275cb2e489d74
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-04-26 13:16:05 +0200

    pokedex 0.0.0

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let actualPage;
+
+loadNextPage();
+
+function loadNextPage() {
+    PokeService.getNextPokemon(actualPage).then(pokemon => {
+        actualPage = pokemon;
+        // console.log(actualPage);
+         display(actualPage) 
+    })
+}
+
+function loadPreviousPage() {
+    PokeService.getPreviousPokemon(actualPage).then(pokemon => {
+        actualPage = pokemon;
+        display(actualPage) 
+    })
+}
+
+function display(page) {   
+    const pokemonList = document.getElementById('pokemon-list');
+    pokemonList.innerHTML = '';
+    const pokemonArr = page.results;
+    for (let i = 0; i < pokemonArr.length; i++) {
+        const element = pokemonArr[i];
+        const pokemonName = element.name;
+        const pokemonText = document.createTextNode(pokemonName);
+        const pokemonUrl = element.url;
+        const pokemonImg = document.createElement('img')
+        pokemonImg.src = pokemonUrl;
+        const newLi = document.createElement('li');
+        newLi.classList.add('poke-li');
+        newLi.appendChild(pokemonText);
+        newLi.appendChild(pokemonImg);
+        pokemonList.appendChild(newLi);
+        console.log(page);
+    }  
+}
