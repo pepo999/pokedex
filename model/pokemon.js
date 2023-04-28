4c365ed8dfc1d11b93afac78e5b76adafa24ac83
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-28 10:09:57 +0200

    update

--- model/pokemon.js
+++ model/pokemon.js
@@ -1,38 +1,38 @@
-class Pokemon {
+// class Pokemon {
 
-    constructor(name, stats = [], types = []) {
-        this.name = name;
-        this.stats = stats;
-        this.types = types;
-    }
+//     constructor(name, stats = [], types = []) {
+//         this.name = name;
+//         this.stats = stats;
+//         this.types = types;
+//     }
 
-    addStats(name, baseValue) {
-        const newStat = new Stat(name, baseValue);
-        this.stats.push(newStat);
-    }
+//     addStats(name, baseValue) {
+//         const newStat = new Stat(name, baseValue);
+//         this.stats.push(newStat);
+//     }
 
-    addTypes(name, url) {
-        const newType = new Type(name, url);
-        this.stats.push(newType);
-    }
+//     addTypes(name, url) {
+//         const newType = new Type(name, url);
+//         this.stats.push(newType);
+//     }
 
 
-}
+// }
 
-class Stat {
+// class Stat {
 
-    constructor(name, baseValue) {
-        this.name = name;
-        this.baseValue = baseValue;
-    }
+//     constructor(name, baseValue) {
+//         this.name = name;
+//         this.baseValue = baseValue;
+//     }
 
-}
+// }
 
-class Type {
+// class Type {
 
-    constructor(name, url) {
-        this.name = name;
-        this.url = url;
-    }
+//     constructor(name, url) {
+//         this.name = name;
+//         this.url = url;
+//     }
 
-}
+// }
