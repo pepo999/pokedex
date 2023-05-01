753fa65df012289bacaf521adedb0f98645d5dfe
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-05-01 09:37:39 +0200

    update

--- detail.js
+++ detail.js
@@ -6,13 +6,6 @@ PokeService.getDetail(pokemonName).then(pokemon => {
     displayPokemon(pokemon);
 })
 
-// PokeService.getDetail(pokemonName).then(pokemonObject => {
-//     console.log('data from db', pokemonObject);
-
-//     const newPokemon = createNewPokemon(pokemonObject);
-//     displayPokemon1(newPokemon)
-// })
-
 function displayPokemon(pokemon) {
     const content = document.getElementById('content');
     const nameText = document.createTextNode(pokemon.id + ' ' + pokemon.name);
@@ -25,46 +18,6 @@ function displayPokemon(pokemon) {
     content.appendChild(textContainer);
     getTypes(pokemon.types)
     content.appendChild(pokemonImg);
-    //change color of background based on type
-    // if (type === 'normal') {
-    //     pokemonImg.style.backgroundColor = 'blanchedalmond'
-    // }
-    // if (type === 'fire') {
-    //     pokemonImg.style.backgroundColor = 'firebrick'
-    // }
-    // if (type === 'water') {
-    //     pokemonImg.style.backgroundColor = 'aqua'
-    // }
-    // if (type === 'grass') {
-    //     pokemonImg.style.backgroundColor = 'forestgreen'
-    // }
-    // if (type === 'bug') {
-    //     pokemonImg.style.backgroundColor = 'darkolivegreen'
-    // }
-    // if (type === 'flying') {
-    //     pokemonImg.style.backgroundColor = 'lightsteelblue'
-    // }
-    // if (type === 'electric') {
-    //     pokemonImg.style.backgroundColor = 'gold'
-    // }
-    // if (type === 'fighting') {
-    //     pokemonImg.style.backgroundColor = 'brown'
-    // }
-    // if (type === 'psychic') {
-    //     pokemonImg.style.backgroundColor = 'mediumorchid'
-    // }
-    // if (type === 'normal') {
-    //     pokemonImg.style.backgroundColor = 'rosybrown'
-    // }
-    // if (type === 'rock') {
-    //     pokemonImg.style.backgroundColor = 'saddlebrown'
-    // }
-    // if (type === 'ghost') {
-    //     pokemonImg.style.backgroundColor = 'darkslateblue'
-    // }
-    // if (type === 'poison') {
-    //     pokemonImg.style.backgroundColor = 'blueviolet'
-    // }
     const detailsContainer = document.getElementById('details-container');
     getStats(pokemon.stats);
     getAbilities(pokemon.abilities);
@@ -82,7 +35,7 @@ typeText = document.createTextNode('type: ');
 typesSpan.appendChild(typeText)
 for (const type of typesArray) {
     const typeTextNode = document.createTextNode(type.type.name + ' ')
-        typesSpan.appendChild(typeTextNode);        
+    typesSpan.appendChild(typeTextNode);        
 }
 content.appendChild(typesSpan);
 }
@@ -132,3 +85,4 @@ function scrollUp() {
 function scrollDown() {
     content.scrollBy(0,50);
 }
+
